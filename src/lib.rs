7b7040a26f21093230e0632b3ac1e21c94657cd5
Simon Théorêt <simonteoret@hotmail.com>
2025-02-05 12:04:20 -0500

    init github

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,148 @@
 mod order;
+mod persistence;
 mod product;
 mod user;
+use derive_more::{Deref, DerefMut, Display, Into};
+use std::error::Error;
+use uuid::Uuid;
 
-/// This module contains the trait associated with CRUD: `Copy`, `Retrieve`, `Update` and `Delete`.
-/// It also has a `Consume` trait. To fully used this module, it is better to implement the NewType
-/// pattern for most strucures.
-mod crud {
+pub use crate::user::User;
 
-    pub struct Context<T>(T);
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display, Deref, DerefMut, Into)]
+pub struct Email(String);
 
-    impl<T> Context<T> {
-        pub fn ref_inner(&self) -> &T {
-            &self.0
-        }
-        pub fn mut_ref_inner(&mut self) -> &mut T {
-            &mut self.0
-        }
-        pub fn into_inner(self) -> T {
-            self.0
+impl AsRef<str> for Email {
+    fn as_ref(&self) -> &str {
+        self.0.as_ref()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display, Deref, DerefMut, Into)]
+pub struct Name(String);
+
+impl AsRef<str> for Name {
+    fn as_ref(&self) -> &str {
+        self.0.as_ref()
+    }
+}
+
+#[derive(
+    Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display, Deref, DerefMut, Into, Copy,
+)]
+pub struct Id(Uuid);
+
+impl Id {
+    pub fn new() -> Self {
+        Id(Uuid::new_v4())
+    }
+}
+
+/// Trait for types that can be created with a fixed size
+pub trait Create<Ctx>: Sized {
+    fn create(ctx: Ctx) -> Self;
+}
+/// Async version of the `Create` trait
+pub trait AsyncCreate<Ctx>: Create<Ctx> {
+    async fn async_create(ctx: Ctx) -> Self {
+        <Self as Create<Ctx>>::create(ctx)
+    }
+}
+
+/// Trait for verifying the type can be created
+pub trait CanBeCreated<Ctx> {
+    fn can_be_created(ctx: Ctx) -> bool;
+}
+
+/// Async version of the `CanBeCreated` trait
+pub trait AsyncCanBeCreated<Ctx>: CanBeCreated<Ctx> {
+    async fn async_can_be_created(ctx: Ctx) -> bool {
+        <Self as CanBeCreated<Ctx>>::can_be_created(ctx)
+    }
+}
+
+/// This trait is used to instantiate types given some conditions are true. The types implementing
+/// this trait must have a method to specify if they can be instantiated given the conditions.
+pub trait SafeCreate<Ctx, Conds>: Create<Ctx> + CanBeCreated<Conds> {
+    fn safe_create(ctx: (Ctx, Conds)) -> Option<Self> {
+        let (create, can) = ctx;
+        if Self::can_be_created(can) {
+            Some(Self::create(create))
+        } else {
+            None
         }
     }
+}
 
-    pub trait Delete<T> {
-        /// Delete data from self
-        fn delete<'a>(&mut self, ctx: Context<T>);
-    }
-
-    pub trait Create<Can, New> {
-        /// Verifies that the conditions a
-        /// re met to create a `Self` object
-        #[allow(unused_variables)]
-        async fn can_be_created<'a>(ctx: Context<Can>) -> bool {
-            false
-        }
-        /// Create a `Self` object
-        async fn new<'a>(ctx: Context<New>) -> Self;
-    }
-
-    pub trait Retrieve<'s, T, K>
-    where
-        T: 's,
-    {
-        /// Retrieves a type T from self
-        async fn retrieve<'a>(&'s self, ctx: Context<K>) -> &'s T;
-        /// Verifies if `self` contains some data
-        #[allow(unused_variables)]
-        async fn contains<'a>(&self, ctx: Context<K>) -> bool {
-            false
+/// Async version of the `SafeCreate` trait.
+pub trait AsyncSafeCreate<Ctx, Conds>: AsyncCreate<Ctx> + AsyncCanBeCreated<Conds> {
+    async fn safe_create(ctx: (Ctx, Conds)) -> Option<Self> {
+        let (create, can) = ctx;
+        if Self::async_can_be_created(can).await {
+            Some(Self::async_create(create).await)
+        } else {
+            None
         }
     }
-    pub trait Consume<T, K> {
-        /// Consumes self in order to retrieve some data
-        async fn consume<'a>(self, ctx: Context<K>) -> T;
-    }
+}
 
-    pub trait Update<K, V> {
-        /// Update some value inside the structure and return the old value
-        async fn update<'a>(&mut self, ctx: Context<(K, V)>) -> V;
+/// Trait for types that can be deleted with a fixed size
+pub trait Delete<Ctx>: Sized {
+    fn delete(self, ctx: Ctx) {
+        std::mem::drop(self)
+    }
+}
+pub trait AsyncDelete<Ctx>: Delete<Ctx> {
+    async fn async_delete(self, ctx: Ctx) {
+        self.delete(ctx)
+    }
+}
+
+/// Trait for verifying the type can be deleted
+pub trait CanBeDeleted<Ctx> {
+    fn can_be_deleted(&self, ctx: Ctx) -> bool;
+}
+
+/// This trait is used to delete types given some that conditions are true. The types implementing
+/// this trait must have a method to specify if they can be instantiated given the conditions.
+pub trait SafeDelete<Ctx, Conds>: Delete<Ctx> + CanBeDeleted<Conds> + Sized {
+    type Err: Error;
+    fn safe_delete(self, ctx: (Ctx, Conds)) -> Option<Self> {
+        let (del_ctx, conditions) = ctx;
+        if self.can_be_deleted(conditions) {
+            self.delete(del_ctx);
+            None
+        } else {
+            Some(self)
+        }
+    }
+}
+
+/// Trait for types that can be updated
+pub trait Update<Ctx> {
+    fn update(&mut self, ctx: Ctx);
+}
+
+/// Trait for verifying the type can be updated
+pub trait CanBeUpdated<Ctx> {
+    fn can_be_updated(&self, ctx: Ctx) -> bool;
+}
+pub trait SafeUpdate<Ctx, Conds>: Update<Ctx> + CanBeUpdated<Conds> {
+    type Err: Error;
+    fn safe_update(&mut self, ctx: (Ctx, Conds)) -> Result<(), Self::Err>;
+}
+
+mod dyn_map {
+    use derive_more::{Deref, DerefMut, Display, From, Into};
+    use std::any::Any;
+    use std::collections::HashMap;
+
+    #[derive(Debug, Display, Deref, DerefMut, Into, From, Default)]
+    /// Dynamic structure capable of holding any kind of data.
+    pub struct DynMap<K>(HashMap<K, Box<dyn Any>>);
+
+    /// Trait for accessing the `DynMap`.
+    pub trait Extended<K> {
+        fn extended_content(&self) -> &DynMap<K>;
+        fn extended_content_mut(&mut self) -> &mut DynMap<K>;
     }
 }
--- src/user/mod.rs
+++ src/user/mod.rs
@@ -1,44 +1,104 @@
-use crate::crud::{Consume, Context, Create, Delete, Retrieve, Update};
-use crate::user::user_registry::UserRegistry;
-use derive_more::{Deref, DerefMut, Display, Into};
+use crate::dyn_map::DynMap;
+use crate::persistence::DBConds;
+use crate::{CanBeCreated, CanBeDeleted, Create, CanBeUpdated};
+use crate::{Email, Id, Name};
+use derive_more::{Display, From, Into};
 
-mod user_registry;
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display, Deref, DerefMut, Into)]
-struct Email(String);
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display, Deref, DerefMut, Into)]
-struct Name(String);
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display)]
 /// What is the state of the Account.
-enum AccountState {
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display)]
+pub enum AccountState {
     Active,
     Dormant,
     Deleted,
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Display)]
-#[display("Name: {name}, Email: {email}, Account state: {account_state}")]
-struct User {
-    name: Name,
-    email: Email,
-    account_state: AccountState,
+#[derive(Debug, Display, From, Into)]
+pub struct InscriptionDate(/*TODO: Add date in here*/);
+
+impl InscriptionDate {
+    pub fn new() -> Self {
+        //TODO: Fill this function with appropriate values
+        InscriptionDate()
+    }
 }
 
-impl<New> Create<UserRegistry, New> for User
-where
-    New: Into<(Name, Email, AccountState)>,
-{
-    async fn can_be_created<'a>(ctx: Context<UserRegistry>) -> bool {
-        todo!()
-    }
-    async fn new<'a>(ctx: Context<New>) -> Self {
-        let (name, email, account_state) = ctx.into_inner().into();
+#[derive(Debug, Display)]
+#[display("Name: {name}, Email: {email}, Inscription date: {inscription_date}, Account state: {account_state}, Id: {id}")]
+pub struct User {
+    name: Name,
+    email: Email,
+    inscription_date: InscriptionDate,
+    account_state: AccountState,
+    id: Id,
+    /// This attribute is not displayed with the `Display` trait
+    ext: DynMap<&'static str>,
+}
+
+impl User {
+    pub fn new(name: Name, email: Email) -> Self {
+        let id = Id::new();
+        let inscription_date = InscriptionDate::new();
+        let account_state = AccountState::Active;
         Self {
             name,
             email,
+            inscription_date,
             account_state,
+            id,
+            ext: DynMap::default(),
         }
+    }
+    pub fn get_name(&self) -> &Name {
+        &self.name
+    }
+    pub fn set_name(&mut self, new: Name) {
+        self.name = new
+    }
+
+    pub fn get_id(&self) -> &Id {
+        &self.id
+    }
+    pub fn set_id(&mut self, new: Id) {
+        self.id = new
+    }
+
+    pub fn get_email(&self) -> &Email {
+        &self.email
+    }
+    pub fn set_email(&mut self, new: Email) {
+        self.email = new
+    }
+
+    pub fn get_inscription_date(&self) -> &InscriptionDate {
+        &self.inscription_date
+    }
+    pub fn set_inscription_date(&mut self, new: InscriptionDate) {
+        self.inscription_date = new
+    }
+
+    pub fn get_account_state(&self) -> &AccountState {
+        &self.account_state
+    }
+    pub fn set_account_state(&mut self, new: AccountState) {
+        self.account_state = new
+    }
+
+}
+impl<'a, DB> CanBeCreated<(DB, &'a Email)> for User
+where
+    DB: DBConds<&'a Email>,
+{
+    fn can_be_created(ctx: (DB, &'a Email)) -> bool {
+        let (db, email) = ctx;
+        if db.exists(email) {
+            return false;
+        } else {
+            true
+        }
+    }
+}
+impl Create<(Name, Email)> for User {
+    fn create(ctx: (Name, Email)) -> Self {
+
     }
 }
